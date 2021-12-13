7c1e8601f5568f81c37bf13d8b65c4176e031d83
lskor <lskor@users.noreply.github.com>
2021-12-13 22:01:52 +0200

    add junit dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,4 +7,9 @@ object Dependencies {
   lazy val KindProjectorVersion = "0.10.3"
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % KindProjectorVersion
 
+  lazy val JunitVersion = "4.12"
+  lazy val junit = "junit" % "junit" % JunitVersion  % "test"
+
+  lazy val JunitInterfaceVersion = "0.11"
+  lazy val junitInterface = "com.novocode" % "junit-interface" % JunitInterfaceVersion % "test"
 }
