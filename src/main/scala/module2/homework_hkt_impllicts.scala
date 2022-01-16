3231e15aa8067a52cada214b18256a1fe87c1846
lskor <lskor@users.noreply.github.com>
2022-01-16 21:50:58 +0200

    - tupleF

--- src/main/scala/module2/homework_hkt_impllicts.scala
+++ src/main/scala/module2/homework_hkt_impllicts.scala
@@ -1,6 +1,6 @@
 package module2
 
-object homework_hkt_impllicts{
+object homework_hkt_implicits{
 
     /**
       * 
@@ -10,14 +10,25 @@ object homework_hkt_impllicts{
       *   val r2 = println(tupleF(list1, list2))
       * 
       */
-    def tupleF[F[_], A, B](fa: F[A], fb: F[B]) = ???
+    def tupleF[F[_], A, B](fa: F[A], fb: F[B])
+                          (implicit bindableA: F[A] => Bindable[F, A], bindableB: F[B] => Bindable[F, B]): F[(A, B)] =
+        bindableA(fa).flatMap(a => bindableB(fb).map(b => (a, b)))
 
 
-    trait Bindable[F[_], A] {
+    abstract class Bindable[F[_], A] {
         def map[B](f: A => B): F[B]
         def flatMap[B](f: A => F[B]): F[B]
     }
 
+    implicit def optToBindableOpt[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+        def map[B](f: A => B): Option[B] = opt.map(f)
+        def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
+    }
+
+    implicit def listToBindableList[A](list: List[A]): Bindable[List, A] = new Bindable[List, A] {
+        def map[B](f: A => B): List[B] = list.map(f)
+        def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
+    }
 
   val optA: Option[Int] = Some(1)
   val optB: Option[Int] = Some(2)
